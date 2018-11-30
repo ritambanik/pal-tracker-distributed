e6f1b50769321c18c68600723fcad14105c88fe8
Leonardo Nelson <leonardo.nelson@wipro.com>
2018-11-30 16:08:25 -0700

    Disabled Redis Connector Bean instantiation in Cloud Environment

--- components/cache-support/src/main/java/io/pivotal/pal/tracker/cachesupport/CacheConfiguration.java
+++ components/cache-support/src/main/java/io/pivotal/pal/tracker/cachesupport/CacheConfiguration.java
@@ -2,6 +2,8 @@ package io.pivotal.pal.tracker.cachesupport;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -10,18 +12,21 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import java.util.Map;
 
 @Configuration
+//@PropertySource(value = "classpath:application.properties")
 public class CacheConfiguration {
 
+
     @Bean
+    @Profile("!cloud")
     JedisConnectionFactory jedisConnectionFactory() {
         return new JedisConnectionFactory();
     }
 
     @Bean
-    public RedisTemplate<String, Map<String, Object>> redisTemplate() {
+    public RedisTemplate<String, Map<String, Object>> redisTemplate(JedisConnectionFactory jedisConnectionFactory) {
         RedisTemplate<String, Map<String, Object>> template = new RedisTemplate<>();
 
-        template.setConnectionFactory(jedisConnectionFactory());
+        template.setConnectionFactory(jedisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 
