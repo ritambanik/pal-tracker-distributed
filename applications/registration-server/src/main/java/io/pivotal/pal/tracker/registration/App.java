58ebefe8e5d52a96d212aab0d6910e1ea54cc2f6
ritambanik <ritam.banik@gmail.com>
2018-12-01 21:48:19 -0500

    Committing all CF specific changes

--- applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
+++ applications/registration-server/src/main/java/io/pivotal/pal/tracker/registration/App.java
@@ -1,4 +1,4 @@
-gitpackage io.pivotal.pal.tracker.registration;
+package io.pivotal.pal.tracker.registration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
