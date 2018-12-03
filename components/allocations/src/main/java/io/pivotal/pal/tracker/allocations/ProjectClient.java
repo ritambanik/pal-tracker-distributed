710cf2d9a7cf8d2f8f0180362b87a18db9b3a09f
ritambanik <ritam.banik@gmail.com>
2018-12-02 20:41:43 -0500

    Changes done to add redis support

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -1,30 +1,118 @@
 package io.pivotal.pal.tracker.allocations;
 
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.web.client.RestOperations;
+import redis.clients.jedis.Jedis;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ProjectClient {
 
     private final RestOperations restOperations;
     private final String registrationServerEndpoint;
-    private final Map<Long, ProjectInfo> projectCacheMap = new ConcurrentHashMap<>();
 
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
-        this.restOperations= restOperations;
+        this.restOperations = restOperations;
         this.registrationServerEndpoint = registrationServerEndpoint;
     }
 
     @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
         ProjectInfo projectInfo = restOperations.getForObject(registrationServerEndpoint + "/projects/" + projectId, ProjectInfo.class);
-        projectCacheMap.put(projectId, projectInfo);
+        Gson gson = new Gson();
+        String json = gson.toJson(projectInfo);
+        setKey(String.valueOf(projectId), json);
         return projectInfo;
     }
 
     private ProjectInfo getProjectFromCache(long projectId) {
-        return projectCacheMap.get(projectId);
+        Gson gson = new Gson();
+        String json = jedis.get(String.valueOf(projectId));
+        ProjectInfo projectInfo = gson.fromJson(json, ProjectInfo.class);
+        return projectInfo;
     }
+
+    private RedisInstanceInfo getInfo() {
+        LOG.log(Level.WARNING, "Getting Redis Instance Info in Spring controller...");
+        // first we need to get the value of VCAP_SERVICES, the environment variable
+        // where connection info is stored
+        String vcap = System.getenv("VCAP_SERVICES");
+        LOG.log(Level.WARNING, "VCAP_SERVICES content: " + vcap);
+
+
+        // now we parse the json in VCAP_SERVICES
+        LOG.log(Level.WARNING, "Using GSON to parse the json...");
+        JsonElement root = new JsonParser().parse(vcap);
+        JsonObject redis = null;
+        if (root != null) {
+            if (root.getAsJsonObject().has("p.redis")) {
+                redis = root.getAsJsonObject().get("p.redis").getAsJsonArray().get(0).getAsJsonObject();
+                LOG.log(Level.WARNING, "instance name: " + redis.get("name").getAsString());
+            } else if (root.getAsJsonObject().has("p-redis")) {
+                redis = root.getAsJsonObject().get("p-redis").getAsJsonArray().get(0).getAsJsonObject();
+                LOG.log(Level.WARNING, "instance name: " + redis.get("name").getAsString());
+            } else {
+                LOG.log(Level.SEVERE, "ERROR: no redis instance found in VCAP_SERVICES");
+            }
+        }
+
+        // then we pull out the credentials block and produce the output
+        if (redis != null) {
+            JsonObject creds = redis.get("credentials").getAsJsonObject();
+            RedisInstanceInfo info = new RedisInstanceInfo();
+            info.setHost(creds.get("host").getAsString());
+            info.setPort(creds.get("port").getAsInt());
+            info.setPassword(creds.get("password").getAsString());
+
+            // the object will be json serialized automatically by Spring web - we just need to return it
+            return info;
+        } else return new RedisInstanceInfo();
+    }
+
+    private Jedis getJedisConnection() {
+        // get our connection info from VCAP_SERVICES
+        RedisInstanceInfo info = getInfo();
+        Jedis jedis = new Jedis(info.getHost(), info.getPort());
+
+        // make the connection
+        jedis.connect();
+
+        // authorize with our password
+        jedis.auth(info.getPassword());
+
+        return jedis;
+    }
+
+    private String setKey(String key, String val) {
+        LOG.log(Level.WARNING, "Called the key set method, going to set key: " + key + " to val: " + val);
+
+        if (jedis == null || !jedis.isConnected()) {
+            jedis = getJedisConnection();
+        }
+        jedis.set(key, val);
+
+        return "Set key: " + key + " to value: " + val;
+    }
+
+    private String getKey(String key) {
+        LOG.log(Level.WARNING, "Called the key get method, going to return val for key: " + key);
+
+        if (jedis == null || !jedis.isConnected()) {
+            jedis = getJedisConnection();
+        }
+
+        return jedis.get(key);
+    }
+
+
+    private Logger LOG = Logger.getLogger(ProjectClient.class.getName());
+
+    private Jedis jedis = null;
+
 }
---
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/RedisInstanceInfo.java
@@ -0,0 +1,31 @@
+package io.pivotal.pal.tracker.allocations;
+
+public class RedisInstanceInfo {
+    private String host;
+    private int port;
+    private String password;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
